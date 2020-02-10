19e8d7cc75e8e95c6b001d30a71b3e5a9d08882e
Serp77 <serp77@rambler.ru>
2020-02-10 09:59:31 +0300

    Moy proect

--- src/test/java/SortSelectedTest.java
+++ src/test/java/SortSelectedTest.java
@@ -10,4 +10,13 @@ public class SortSelectedTest {
         int[] expect = new int[] {1, 2, 3, 4, 5};
         assertThat(result, is(expect));
     }
+
+
+    @Test
+    public void whenSort1() {
+        int[] input = new int[] {1, 2, 3};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 2, 3};
+        assertThat(result, is(expect));
+    }
 }
