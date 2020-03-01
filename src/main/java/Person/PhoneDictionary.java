40f1203273601e3572337c1bc40f1ee30848682d
Serp77 <serp77@rambler.ru>
2020-03-01 20:59:38 +0300

    Phone

---
+++ src/main/java/Person/PhoneDictionary.java
@@ -0,0 +1,24 @@
+package Person;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for ( int i = 0; i < result.size(); i++)
+            if (result.contains(key));
+        return result;
+    }
+}
